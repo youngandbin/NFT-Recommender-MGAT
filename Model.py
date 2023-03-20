88ceaebb964ed293ca4c81b61270e7570fb0cb35
YOUNG <young@unist.ac.kr>
2023-03-20 12:42:20 +0900

    Add files via upload

--- Model.py
+++ Model.py
@@ -34,22 +34,11 @@ class MMMO(torch.nn.Module):
         self.tr_gnn = GAT(self.tr_feat, self.user_features, self.edge_index, batch_size, num_user, num_item, dim_x, DROPOUT, dim_latent=64)
 
         self.id_embedding = nn.Embedding(num_user+num_item, dim_x)
-        # print('self.id_embedding: ', self.id_embedding)
         nn.init.xavier_normal_(self.id_embedding.weight)
-
-        #self.id_embedding = nn.init.xavier_normal_(torch.rand((num_user+num_item, dim_x), requires_grad=True)).cuda()
         self.result_embed = nn.init.xavier_normal_(torch.rand((num_user+num_item, dim_x))).cuda()
 
-        # read dataset/indices.npy
-        # self.indices = np.load(path+'indices.npy', allow_pickle=True)
-
-        # linear layer
+        # linear layers
         self.MLP_price = MLP_price(dim_in=dim_x*2, dim_out=1)
-
-        # cluster dict
-        self.cluster_dict = cluster_dict
-
-        # linear layer (for attention)
         self.q_fc = nn.Linear(dim_x, dim_x, bias=False) # (d_model, d_model)
         self.k_fc = nn.Linear(dim_x, dim_x, bias=False) # (d_model, d_model)
         self.v_fc = nn.Linear(dim_x, dim_x, bias=False) # (d_model, d_model)
@@ -59,79 +48,66 @@ class MMMO(torch.nn.Module):
 
         # print('user_nodes: ', user_nodes.shape) # torch.Size([2048])
         # print('pos_items: ', pos_items.shape) # torch.Size([2048])
-        # print('neg_items: ', neg_items.shape) # torch.Size([2048])        
+        # print('neg_items: ', neg_items.shape) # torch.Size([2048])      
+          
         v_rep = self.v_gnn(self.id_embedding)
         t_rep = self.t_gnn(self.id_embedding) 
         p_rep = self.p_gnn(self.id_embedding)
-        tr_rep = self.tr_gnn(self.id_embedding)
+        tr_rep = self.tr_gnn(self.id_embedding) # torch.Size([num_user+num_item, 512])
         self.v_representation = v_rep
         self.t_representation = t_rep
         self.p_representation = p_rep
         self.tr_representation = tr_rep
-        # print('tr_rep: ', tr_rep.shape) # torch.Size([num_user+num_item, 512])
 
-        representation = (v_rep + t_rep + p_rep + tr_rep) / 4 #torch.max_pool2d((v_rep, a_rep, t_rep))#max()#torch.cat((v_rep, a_rep, t_rep), dim=1)
-        # print('representation: ', representation.shape) # torch.Size([num_user+num_item, 512])
+        representation = (v_rep + t_rep + p_rep + tr_rep) / 4 # torch.Size([num_user+num_item, 512])
         self.result_embed = representation
         user_tensor = representation[user_nodes]
         pos_tensor = representation[pos_items]
         neg_tensor = representation[neg_items]
 
         # QUERY
-        Q = user_tensor
-        # print('Q: ', Q.shape) # torch.Size([2048, 512])
+        Q = user_tensor # torch.Size([2048, 512])
         # KEY, VALUE
         pos_tensor_v = v_rep[pos_items]
         pos_tensor_t = t_rep[pos_items]
         pos_tensor_p = p_rep[pos_items]
-        pos_tensor_tr = tr_rep[pos_items]
-        # print('pos_tensor_tr: ', pos_tensor_tr.shape) # torch.Size([2048, 512])
+        pos_tensor_tr = tr_rep[pos_items] # torch.Size([2048, 512])
 
         # create a matrix where each row is the average of pos_tensor_v, pos_tensor_t, pos_tensor_p, pos_tensor_tr
         K = torch.stack([pos_tensor_v.mean(dim=0), pos_tensor_t.mean(dim=0), pos_tensor_p.mean(dim=0), pos_tensor_tr.mean(dim=0)], dim=0)
-        # copy K
-        V = K.clone()
-        # print('K: ', K.shape) # torch.Size([4, 512])
+        V = K.clone() # torch.Size([4, 512])
 
         # transform
-        Q = self.q_fc(Q)
-        # print('Q transformed: ', Q.shape) # torch.Size([2048, 512])
-        K = self.k_fc(K)
-        # print('K transformed: ', K.shape) # torch.Size([4, 512])
+        Q = self.q_fc(Q) # torch.Size([2048, 512])
+        K = self.k_fc(K) # torch.Size([4, 512])
         V = self.v_fc(V)
 
         # calculate attention
-        attention = torch.matmul(Q, K.transpose(1, 0)) / np.sqrt(K.shape[1])
-        # print('attention: ', attention.shape) # torch.Size([2048, 4])
+        attention = torch.matmul(Q, K.transpose(1, 0)) / np.sqrt(K.shape[1]) # torch.Size([2048, 4])
         attention = F.softmax(attention, dim=1)
-        attention = torch.matmul(attention, V)
-        # print('attention: ', attention.shape) # torch.Size([2048, 512])
+        attention = torch.matmul(attention, V) # torch.Size([2048, 512])
         user_tensor = attention
-
         
-
-        # 1) BPR loss
+        # 1) BPR pred
         pos_scores = torch.sum(user_tensor * pos_tensor, dim=1)
-        neg_score = torch.sum(user_tensor * neg_tensor, dim=1)
+        neg_scores = torch.sum(user_tensor * neg_tensor, dim=1)
         
-        # 2) Price loss
+        # 2) Price pred
         user_pos_tensor = torch.cat((user_tensor, pos_tensor), dim=1)
-        # linear layer 
         pred_price = self.MLP_price(user_pos_tensor)
         
-        return pos_scores, neg_score, representation, pred_price
+        return pos_scores, neg_scores, representation, pred_price
 
     def loss(self, data):
         user, pos_items, neg_items, labels = data
-        pos_scores, neg_scores,representation, pred_price = self.forward(user.cuda(), pos_items.cuda(), neg_items.cuda())
+        pos_scores, neg_scores, representation, pred_price = self.forward(user.cuda(), pos_items.cuda(), neg_items.cuda())
 
         # 1) BPR loss
         loss_value = -torch.sum(torch.log2(torch.sigmoid(pos_scores - neg_scores)))
         reg = (torch.norm(representation[user])**2
-        +torch.norm(representation[pos_items])**2
-        +torch.norm(representation[neg_items])**2) / 2
-        loss_reg = self.reg_parm*reg/self.batch_size
-
+                +torch.norm(representation[pos_items])**2
+                +torch.norm(representation[neg_items])**2) / 2
+        loss_reg = self.reg_parm*reg / self.batch_size
         loss_BPR = loss_value + loss_reg
 
         # 2) BCE loss
@@ -141,24 +117,15 @@ class MMMO(torch.nn.Module):
 
 
     def accuracy(self, dataset, indices, topk=10, neg_num=100):
-        all_set = set(list(np.arange(neg_num)))
-        sum_pre = 0.0
-        sum_recall = 0.0
-        sum_ndcg = 0.0
-        sum_item = 0
-        # bar = tqdm(total=len(dataset))
-        recall_list = np.array([0.0, 0.0, 0.0, 0.0])
-        ndcg_list = np.array([0.0, 0.0, 0.0, 0.0])
+        all_set = set(list(np.arange(neg_num))) 
+        num_user = len(dataset)
+        recall_list = np.array([0.0, 0.0, 0.0, 0.0]) # topK=5,10,15,20
+        ndcg_list = np.array([0.0, 0.0, 0.0, 0.0])   # topK=5,10,15,20
 
-
-        for data in dataset:
-            # bar.update(1)
-
-            sum_item += 1
+        for data in dataset: # 유저 한 명 씩 loop
             user = data[0]
             pos_items = data[1:]
-      
-            # minus from self.indices to pos_items
+            # popularity-based negative sampling
             neg_items = [x for x in indices if x not in pos_items]
             neg_items = neg_items[:neg_num]
             neg_items = list(neg_items)
@@ -175,22 +142,18 @@ class MMMO(torch.nn.Module):
             pos_score = torch.sum(pos_v_embed*user_embed, dim=1)
             neg_score = torch.sum(neg_v_embed*user_embed, dim=1)
 
-             # make the topk items using the price score
+            # # make the topk items using the price score
+            # pos_price = self.MLP_price(torch.cat((user_embed.repeat(len(pos_items),1), pos_v_embed), dim=1))
+            # neg_price = self.MLP_price(torch.cat((user_embed.repeat(neg_num,1), neg_v_embed), dim=1))
 
-            pos_price = self.MLP_price(torch.cat((user_embed.repeat(len(pos_items),1), pos_v_embed), dim=1))
-            neg_price = self.MLP_price(torch.cat((user_embed.repeat(neg_num,1), neg_v_embed), dim=1))
-
-
-            # make the topk items using the rank score
-            # save the result from topk 5 to topk 10
+            ###################################### select topK based on scores ######################################
             for k, topk in enumerate(range(5, 21, 5)):
-                _, index_of_rank_list = torch.topk(torch.cat((neg_score, pos_score)), topk)
+                _, index_of_rank_list = torch.topk(torch.cat((neg_score, pos_score)), topk) # 상위 K=5개의 idx (e.g., [0, 10, 101, 130, 315])
+                # Recall
                 index_set = set([iofr.cpu().item() for iofr in index_of_rank_list])
-                
-                num_hit = len(index_set.difference(all_set))
-                # sum_pre += float(num_hit/topk)
-      
+                num_hit = len(index_set.difference(all_set))                                # 위 idx 중에서 100 이상만 pos_items (e.g., [130, 315]])
                 recall = float(num_hit/num_pos)
+                # NDCG
                 ndcg_score = 0.0
                 for i in range(num_pos):
                     label_pos = neg_num + i
@@ -198,17 +161,16 @@ class MMMO(torch.nn.Module):
                         index = list(index_of_rank_list.cpu().numpy()).index(label_pos)
                         ndcg_score = ndcg_score + math.log(2) / math.log(index + 2)
                 ndcg = ndcg_score/num_pos
-
                 # append to the list
                 recall_list[k] += recall
                 ndcg_list[k] += ndcg
 
-        return recall_list/sum_item, ndcg_list/sum_item
+        return recall_list/num_user, ndcg_list/num_user
 
     def attention_score(self, dataset):
         attention_score = {}
         for data in dataset:
-            # bar.update(1)
+
             user = data[0]
             pos_items = data[1]
 
@@ -225,14 +187,11 @@ class MMMO(torch.nn.Module):
             pos_tensor_tr = tr_rep[pos_items]
 
             K = torch.stack([pos_tensor_v, pos_tensor_t, pos_tensor_p, pos_tensor_tr], dim=0)
-            
             V = K.clone()
 
-            Q = self.q_fc(Q)
-            # print('Q transformed: ', Q.shape) # torch.Size([2048, 512])
-            K = self.k_fc(K)
-            # print('K transformed: ', K.shape) # torch.Size([4, 512])
-            V = self.v_fc(V)
+            Q = self.q_fc(Q) # torch.Size([2048, 512])
+            K = self.k_fc(K) # torch.Size([4, 512])
+            V = self.v_fc(V) # torch.Size([4, 512])
 
             attention = torch.matmul(Q, K.transpose(1, 0)) / np.sqrt(K.shape[1])
             attention_score[user] = attention.tolist()
@@ -331,13 +290,11 @@ class GRAPH(torch.nn.Module):
         sum_recall = 0.0
         sum_ndcg = 0.0
         sum_item = 0
-        # bar = tqdm(total=len(dataset))
         recall_list = np.array([0.0, 0.0, 0.0, 0.0])
         ndcg_list = np.array([0.0, 0.0, 0.0, 0.0])
 
 
         for data in dataset:
-            # bar.update(1)
 
             sum_item += 1
             user = data[0]
